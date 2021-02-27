49d2b7f54d1236247c93196f815305a3e480e2d6
Shubhamr06 <32364476+Shubhamr06@users.noreply.github.com>
2021-02-27 19:34:21 +0530

    Add files via upload
    This program will take input from the user until user enter correct no i.e greater than 100. ( if user enter less than 100 than program will not stop and again ask input from user)
    
    -
    SR

---
+++ p1.py
@@ -0,0 +1,16 @@
+
+
+while(True):
+    
+    n=int(input('enter no: '))
+    
+    if n>100:
+        
+        print('congrats you have entered correct no')
+        
+        break
+        
+    else:
+        
+        print('please enter correct no')
+        continue
